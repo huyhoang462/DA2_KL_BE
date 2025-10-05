99aabdcdf1f42ac2661668e7175954be4a6081a5
huyhoang462 <22520462@gm.uit.edu.vn>
2025-10-05 21:38:22 +0700

    feat(auth): add signup feature

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -11,22 +11,26 @@ const handleLogin = async (req, res) => {
     if (!user) {
       console.log("User  found");
 
-      return res.status(400).json({ message: "User not found" });
+      return res.status(401).json({ message: "Invalid email or password" });
     }
     const isMatch = await bcrypt.compare(password, user.passwordHash);
     if (!isMatch) {
-      return res.status(400).json({ message: "Password incorrect " });
+      return res.status(401).json({ message: "Invalid email or password " });
     }
 
-    const token = jwt.sign(
-      { userId: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-    res.json({
+    const userForToken = {
+      email: user.email,
+      id: user._id,
+      role: user.role,
+    };
+
+    const token = jwt.sign(userForToken, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({
       token,
       user: {
-        id: user._id,
         email: user.email,
         name: user.name,
         role: user.role,
@@ -37,6 +41,44 @@ const handleLogin = async (req, res) => {
   }
 };
 
+const handleSignup = async (req, res) => {
+  const { email, password, name, role, phone, confirmPassword } = req.body;
+  // Validate input
+  if (!email || !password || !name || !role || !phone || !confirmPassword) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  if (password !== confirmPassword) {
+    return res.status(400).json({ message: "Passwords do not match" });
+  }
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "Password must be at least 6 characters long" });
+  }
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      email,
+      passwordHash,
+      name,
+      role,
+      phone,
+    });
+    const savedUser = await newUser.save();
+
+    res.status(201).json(savedUser);
+  } catch (err) {
+    console.error("Signup Error:", err);
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
+
 module.exports = {
   login: handleLogin,
+  signup: handleSignup,
 };
