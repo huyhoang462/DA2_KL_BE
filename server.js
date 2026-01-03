3d594d5175c11e843c77518b43973a731018428c
huyhoang462 <22520462@gm.uit.edu.vn>
2026-01-03 16:31:04 +0700

    chore: CORS config

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
+      "https://shine-ticket.vercel.app",
       process.env.FRONTEND_URL,
     ].filter(Boolean),
     credentials: true, // BẮT BUỘC để nhận Cookies/Token
